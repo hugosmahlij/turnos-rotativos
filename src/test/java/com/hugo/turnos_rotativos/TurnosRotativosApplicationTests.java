50f3e52957cd2fcedd41b29a8419e0383d856eda
Hugo <smahlij@gmail.com>
2024-08-30 17:41:08 -0300

    Initial commit

---
+++ src/test/java/com/hugo/turnos_rotativos/TurnosRotativosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugo.turnos_rotativos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnosRotativosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
